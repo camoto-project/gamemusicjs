d1705432e007fded81dad941c340c95877dc62f3
Adam Nielsen <malvineous@shikadi.net>
2020-11-29 12:38:35 +1000

    test: Update test code to better handle events being moved between tracks
    Now we look for the events we want on any track and check the timing, rather
    than looking at a specific track at a specific time and expecting the event
    to be there.
    
    This allows the format handlers to move events between tracks as they see fit,
    e.g. splitting by OPL or MIDI channel, and the tests will still work.

--- test/index.js
+++ test/index.js
@@ -20,6 +20,7 @@
 const assert = require('assert');
 
 const TestUtil = require('./util.js');
+const UtilMusic = require('../src/utl-music.js');
 const GameMusic = require('../index.js');
 const Music = GameMusic.Music;
 
@@ -48,12 +49,333 @@ const identifyConflicts = {
 
 // Override the default colours so we can actually see them
 var colors = require('mocha/lib/reporters/base').colors;
-colors['diff added'] = '1;33';
+colors['diff added'] = '1;32';
 colors['diff removed'] = '1;31';
 colors['green'] = '1;32';
 colors['fail'] = '1;31';
 colors['error message'] = '1;31';
 colors['error stack'] = '1;37';
+
+// Amount to transpose each note by depending on track.  This is so we can find
+// the notes again even if the format handler moves them to a different track.
+const transpose = {
+	[Music.ChannelType.OPLT]: 0,
+	[Music.ChannelType.OPLF]: 25,
+	[Music.ChannelType.OPLR]: 60,
+	[Music.ChannelType.MIDI]: 90,
+	[Music.ChannelType.PCM]: 145,
+};
+
+function createDefaultMusic(md)
+{
+	// This is what we expect the default song in any given format to
+	// look like.
+
+	/**
+	 * Add a new track with some notes using the specified instrument.
+	 *
+	 * This ensures every instrument in the song is used, with the actual
+	 * instruments dependent on what the file format supports.
+	 */
+	function createTrack(instrument, channelType)
+	{
+		const ts = transpose[channelType] || 0;
+
+		let track = new Music.Track();
+
+		track.events.push(
+			new Music.NoteOnEvent({
+				frequency: 440 + ts, // A-4
+				velocity: 1,
+				instrument: instrument,
+			})
+		);
+
+		track.events.push(
+			new Music.DelayEvent({
+				ticks: 10,
+			})
+		);
+
+		track.events.push(
+			new Music.NoteOffEvent()
+		);
+
+		track.events.push(
+			new Music.NoteOnEvent({
+				frequency: 92.5 + ts, // F#2
+				velocity: 0.5,
+				instrument: instrument,
+			})
+		);
+
+		track.events.push(
+			new Music.DelayEvent({
+				ticks: 10,
+			})
+		);
+
+		track.events.push(
+			new Music.NoteOffEvent()
+		);
+
+		return track;
+	}
+
+	let defaultMusic = new Music();
+	defaultMusic.initialTempo.usPerTick = 1000;
+
+	let pattern = new Music.Pattern();
+
+	// Figure out what type of instruments to use depending on what the
+	// format supports.
+	let channelTypes = [];
+	for (const cm of md.caps.channelMap) {
+		for (const mapping of cm.mappings) {
+			for (const target of mapping.channels) {
+				channelTypes[target.type] = true;
+			}
+		}
+	}
+
+	if (channelTypes[Music.ChannelType.OPLT]) {
+		let inst = new Music.Patch.OPL({
+			title: 'TEST',
+			slot: [
+				{
+					enableTremolo: 1,
+					enableVibrato: 1,
+					enableSustain: 1,
+					enableKSR: 0,
+					freqMult: 2,
+					scaleLevel: 3,
+					outputLevel: 4,
+					attackRate: 5,
+					decayRate: 6,
+					sustainRate: 7,
+					releaseRate: 8,
+					waveSelect: 6,
+				}, {
+					enableTremolo: 1,
+					enableVibrato: 0,
+					enableSustain: 1,
+					enableKSR: 0,
+					freqMult: 3,
+					scaleLevel: 2,
+					outputLevel: 5,
+					attackRate: 6,
+					decayRate: 7,
+					sustainRate: 8,
+					releaseRate: 9,
+					waveSelect: 5,
+				},
+			],
+			feedback: 4,
+			connection: 0,
+		});
+		defaultMusic.patches.push(inst);
+
+		defaultMusic.trackConfig.push(new Music.TrackConfiguration({
+			channelType: Music.ChannelType.OPLT,
+			channelIndex: 0,
+		}));
+
+		// Add some notes using this instrument to a new track.
+		let track = createTrack(
+			defaultMusic.patches.length - 1,
+			Music.ChannelType.OPLT
+		);
+		pattern.tracks.push(track);
+	}
+
+	if (channelTypes[Music.ChannelType.OPLF]) {
+		let inst = new Music.Patch.OPL({
+			slot: [
+				{
+					enableTremolo: 1,
+					enableVibrato: 1,
+					enableSustain: 1,
+					enableKSR: 0,
+					freqMult: 2,
+					scaleLevel: 3,
+					outputLevel: 4,
+					attackRate: 5,
+					decayRate: 6,
+					sustainRate: 7,
+					releaseRate: 8,
+					waveSelect: 6,
+				}, {
+					enableTremolo: 1,
+					enableVibrato: 0,
+					enableSustain: 1,
+					enableKSR: 0,
+					freqMult: 3,
+					scaleLevel: 2,
+					outputLevel: 5,
+					attackRate: 6,
+					decayRate: 7,
+					sustainRate: 8,
+					releaseRate: 9,
+					waveSelect: 5,
+				}, {
+					enableTremolo: 0,
+					enableVibrato: 1,
+					enableSustain: 0,
+					enableKSR: 1,
+					freqMult: 0,
+					scaleLevel: 1,
+					outputLevel: 6,
+					attackRate: 7,
+					decayRate: 8,
+					sustainRate: 9,
+					releaseRate: 10,
+					waveSelect: 4,
+				}, {
+					enableTremolo: 0,
+					enableVibrato: 0,
+					enableSustain: 1,
+					enableKSR: 1,
+					freqMult: 1,
+					scaleLevel: 0,
+					outputLevel: 7,
+					attackRate: 8,
+					decayRate: 9,
+					sustainRate: 10,
+					releaseRate: 11,
+					waveSelect: 3,
+				},
+			],
+			feedback: 3,
+			connection: 1,
+		});
+		defaultMusic.patches.push(inst);
+
+		defaultMusic.trackConfig.push(new Music.TrackConfiguration({
+			channelType: Music.ChannelType.OPLF,
+			channelIndex: 1,
+		}));
+
+		// Add some notes using this instrument to a new track.
+		let track = createTrack(
+			defaultMusic.patches.length - 1,
+			Music.ChannelType.OPLF
+		);
+
+		pattern.tracks.push(track);
+
+		// Do the next step in a new track so it's easier to match the track count,
+		// since parseOPL() splits global events into their own tracks.
+		track = new Music.Track();
+
+		// We have to enable OPL3 mode to use 4-op instruments, so add this event
+		// in front of the note events.
+		track.events.unshift(new Music.ConfigurationEvent({
+			option: Music.ConfigurationEvent.Option.EnableOPL3,
+			value: true,
+		}));
+
+		defaultMusic.trackConfig.push(new Music.TrackConfiguration({
+			channelType: Music.ChannelType.OPLT,
+			channelIndex: 0,
+		}));
+		pattern.tracks.push(track);
+	}
+
+	if (channelTypes[Music.ChannelType.OPLR]) {
+		let inst = new Music.Patch.OPL({
+			slot: [
+				{
+					enableTremolo: 1,
+					enableVibrato: 1,
+					enableSustain: 0,
+					enableKSR: 1,
+					freqMult: 8,
+					scaleLevel: 3,
+					outputLevel: 4,
+					attackRate: 5,
+					decayRate: 6,
+					sustainRate: 7,
+					releaseRate: 8,
+					waveSelect: 3,
+				},
+			],
+			feedback: 6,
+			connection: 1,
+		});
+		defaultMusic.patches.push(inst);
+
+		defaultMusic.trackConfig.push(new Music.TrackConfiguration({
+			channelType: Music.ChannelType.OPLR,
+			channelIndex: GameMusic.UtilOPL.Rhythm.HH,
+		}));
+
+		// Add some notes using this instrument to a new track.
+		let track = createTrack(
+			defaultMusic.patches.length - 1,
+			Music.ChannelType.OPLR
+		);
+
+		// We have to enable rhythm mode to use rhythm instruments, so add this
+		// event in front of the note events.
+		track.events.unshift(new Music.ConfigurationEvent({
+			option: Music.ConfigurationEvent.Option.EnableRhythm,
+			value: true,
+		}));
+
+		pattern.tracks.push(track);
+	}
+
+	if (channelTypes[Music.ChannelType.MIDI]) {
+		let inst = new Music.Patch.MIDI({
+			midiBank: 0,
+			midiPatch: 1,
+		});
+		defaultMusic.patches.push(inst);
+
+		defaultMusic.trackConfig.push(new Music.TrackConfiguration({
+			channelType: Music.ChannelType.MIDI,
+			channelIndex: 0,
+		}));
+
+		// Add some notes using this instrument to a new track.
+		let track = createTrack(
+			defaultMusic.patches.length - 1,
+			Music.ChannelType.MIDI
+		);
+		pattern.tracks.push(track);
+	}
+
+	if (channelTypes[Music.ChannelType.PCM]) {
+		let inst = new Music.Patch.PCM({
+			sampleRate: 8000,
+		});
+		defaultMusic.patches.push(inst);
+
+		defaultMusic.trackConfig.push(new Music.TrackConfiguration({
+			channelType: Music.ChannelType.PCM,
+			channelIndex: 0,
+		}));
+
+		// Add some notes using this instrument to a new track.
+		let track = createTrack(
+			defaultMusic.patches.length - 1,
+			Music.ChannelType.PCM
+		);
+		pattern.tracks.push(track);
+	}
+
+	// Add our pattern with all the tracks and notes to the song.
+	defaultMusic.patterns.push(pattern);
+
+	const mdTags = Object.keys(md.caps.tags);
+	if (mdTags.length > 0) {
+		mdTags.forEach(mdTag => {
+			defaultMusic.tags[mdTag] = `${mdTag} goes here`;
+		});
+	}
+
+	return defaultMusic;
+}
 
 const allHandlers = GameMusic.listHandlers();
 allHandlers.forEach(handler => {
@@ -63,11 +385,14 @@ allHandlers.forEach(handler => {
 		return;
 	}
 
+	// Do this outside of the test handler so it's available when constructing
+	// the tests, so we can do things like create a test for each instrument.
+	const defaultMusic = createDefaultMusic(md);
+
 	let testutil = new TestUtil(md.id);
 
 	describe(`Standard tests for ${md.title} [${md.id}]`, function() {
 		let content = {};
-		let defaultMusic = new Music();
 
 		// Test the metadata first because we need it for the test file.
 		describe('metadata()', function() {
@@ -110,267 +435,98 @@ allHandlers.forEach(handler => {
 					'default',
 				]);
 
-				// This is what we expect the default song in any given format to
-				// look like.
-
-				let track = new Music.Track();
-
-				track.events.push(
-					new Music.NoteOnEvent({
-						frequency: 440, // A-4
-						velocity: 1,
-						instrument: 0,
-					})
-				);
-
-				track.events.push(
-					new Music.DelayEvent({
-						ticks: 10,
-					})
-				);
-
-				track.events.push(
-					new Music.NoteOffEvent()
-				);
-
-				track.events.push(
-					new Music.NoteOnEvent({
-						frequency: 92.5, // F#2
-						velocity: 0.5,
-						instrument: 0,
-					})
-				);
-
-				track.events.push(
-					new Music.DelayEvent({
-						ticks: 10,
-					})
-				);
-
-				track.events.push(
-					new Music.NoteOffEvent()
-				);
-
-				let pattern = new Music.Pattern();
-				pattern.tracks.push(track);
-
-				defaultMusic.patterns.push(pattern);
-
-				// Figure out what type of instruments to use depending on what the
-				// format supports.
-				let channelTypes = [];
-				for (const cm of md.caps.channelMap) {
-					for (const mapping of cm.mappings) {
-						for (const target of mapping.channels) {
-							channelTypes[target.type] = true;
-						}
-					}
-				}
-
-				if (channelTypes[Music.ChannelType.OPLT]) {
-					let inst = new Music.Patch.OPL({
-						slot: [
-							{
-								enableTremolo: 1,
-								enableVibrato: 1,
-								enableSustain: 1,
-								enableKSR: 0,
-								freqMult: 2,
-								scaleLevel: 3,
-								outputLevel: 4,
-								attackRate: 5,
-								decayRate: 6,
-								sustainRate: 7,
-								releaseRate: 8,
-								waveSelect: 6,
-							}, {
-								enableTremolo: 1,
-								enableVibrato: 0,
-								enableSustain: 1,
-								enableKSR: 0,
-								freqMult: 3,
-								scaleLevel: 4,
-								outputLevel: 5,
-								attackRate: 6,
-								decayRate: 7,
-								sustainRate: 8,
-								releaseRate: 9,
-								waveSelect: 5,
-							},
-						],
-						feedback: 4,
-						connection: 0,
-					});
-					defaultMusic.patches.push(inst);
-
-					defaultMusic.trackConfig.push(new Music.TrackConfiguration({
-						channelType: Music.ChannelType.OPLT,
-						channelIndex: 0,
-					}));
-				}
-
-				if (channelTypes[Music.ChannelType.OPLF]) {
-					let inst = new Music.Patch.OPL({
-						slot: [
-							{
-								enableTremolo: 1,
-								enableVibrato: 1,
-								enableSustain: 1,
-								enableKSR: 0,
-								freqMult: 2,
-								scaleLevel: 3,
-								outputLevel: 4,
-								attackRate: 5,
-								decayRate: 6,
-								sustainRate: 7,
-								releaseRate: 8,
-								waveSelect: 6,
-							}, {
-								enableTremolo: 1,
-								enableVibrato: 0,
-								enableSustain: 1,
-								enableKSR: 0,
-								freqMult: 3,
-								scaleLevel: 4,
-								outputLevel: 5,
-								attackRate: 6,
-								decayRate: 7,
-								sustainRate: 8,
-								releaseRate: 9,
-								waveSelect: 5,
-							}, {
-								enableTremolo: 0,
-								enableVibrato: 1,
-								enableSustain: 0,
-								enableKSR: 1,
-								freqMult: 0,
-								scaleLevel: 5,
-								outputLevel: 6,
-								attackRate: 7,
-								decayRate: 8,
-								sustainRate: 9,
-								releaseRate: 10,
-								waveSelect: 4,
-							}, {
-								enableTremolo: 0,
-								enableVibrato: 0,
-								enableSustain: 0,
-								enableKSR: 0,
-								freqMult: 1,
-								scaleLevel: 6,
-								outputLevel: 7,
-								attackRate: 8,
-								decayRate: 9,
-								sustainRate: 10,
-								releaseRate: 11,
-								waveSelect: 3,
-							},
-						],
-						feedback: 3,
-						connection: 1,
-					});
-					defaultMusic.patches.push(inst);
-
-					defaultMusic.trackConfig.push(new Music.TrackConfiguration({
-						channelType: Music.ChannelType.OPLF,
-						channelIndex: 3,
-					}));
-				}
-
-				if (channelTypes[Music.ChannelType.OPLR]) {
-					let inst = new Music.Patch.OPL({
-						slot: [
-							{
-								enableTremolo: 1,
-								enableVibrato: 1,
-								enableSustain: 0,
-								enableKSR: 1,
-								freqMult: 8,
-								scaleLevel: 3,
-								outputLevel: 4,
-								attackRate: 5,
-								decayRate: 6,
-								sustainRate: 7,
-								releaseRate: 8,
-								waveSelect: 3,
-							},
-						],
-						feedback: 6,
-						connection: 1,
-					});
-					defaultMusic.patches.push(inst);
-
-					defaultMusic.trackConfig.push(new Music.TrackConfiguration({
-						channelType: Music.ChannelType.OPLR,
-						channelIndex: GameMusic.UtilOPL.Rhythm.HH,
-					}));
-				}
-
-				if (channelTypes[Music.ChannelType.MIDI]) {
-					let inst = new Music.Patch.MIDI({
-						midiBank: 0,
-						midiPatch: 1,
-					});
-					defaultMusic.patches.push(inst);
-
-					defaultMusic.trackConfig.push(new Music.TrackConfiguration({
-						channelType: Music.ChannelType.MIDI,
-						channelIndex: 0,
-					}));
-				}
-
-				if (channelTypes[Music.ChannelType.PCM]) {
-					let inst = new Music.Patch.PCM({
-						sampleRate: 8000,
-					});
-					defaultMusic.patches.push(inst);
-
-					defaultMusic.trackConfig.push(new Music.TrackConfiguration({
-						channelType: Music.ChannelType.PCM,
-						channelIndex: 0,
-					}));
-				}
-
-				const mdTags = Object.keys(md.caps.tags);
-				if (mdTags.length > 0) {
-					mdTags.forEach(mdTag => {
-						defaultMusic.tags[mdTag] = `${mdTag} goes here`;
-					});
-				}
-
 			});
 
 			describe('parse()', function() {
 				let music;
+
+				assert.ok(md.caps.supportedEvents, `caps.supportedEvents missing for ${md.id}.`);
+				const hasNoteOn = md.caps.supportedEvents.some(ev => ev instanceof Music.NoteOnEvent);
 
 				before('should parse correctly', function() {
 					music = handler.parse(content.default);
 					assert.notStrictEqual(music, undefined);
 					assert.notStrictEqual(music, null);
 
-					assert.ok(music.patterns, 'music.patterns must be an array.');
-					assert.ok(music.patterns[0].tracks, 'music.patterns[0].tracks must be an array.');
+					if (hasNoteOn) {
+						assert.ok(music.patterns, 'music.patterns must be an array.');
+						assert.ok(music.patterns[0].tracks, 'music.patterns[0].tracks must be an array.');
+					}
 				});
 
-				it('should have the expected events', function() {
-					const events = music.patterns[0].tracks[0].events;
-					assert.ok(events[0], 'parse() returned patterns[0].tracks[0].events[0] as invalid');
-					assert.equal(events[0].type, Music.NoteOnEvent);
-					assert.equal(events[1].type, Music.DelayEvent);
-					assert.equal(events[2].type, Music.NoteOffEvent);
-					assert.equal(events[3].type, Music.NoteOnEvent);
-					assert.equal(events[4].type, Music.DelayEvent);
-					assert.equal(events[5].type, Music.NoteOffEvent);
+				// Tests for each channel type.
+				for (const tcExp of defaultMusic.trackConfig) {
+					const channelTypeText = Music.ChannelType.toString(tcExp.channelType);
+					describe(`should support ${channelTypeText} channels`, function() {
 
-					TestUtil.almostEqual(events[0].frequency, 440, 0.1);
-					TestUtil.almostEqual(events[4].frequency, 92.5, 0.1);
+						before(`merge patterns and tracks`, function() {
+							if (hasNoteOn) {
+								assert.ok(music.patterns, 'Expected one or more patterns, found none.');
+
+								const mergedEvents = UtilMusic.mergePatternsAndTracks(music.patterns);
+
+								// Convert events into timed list
+								this.timedEvents = [];
+								let t = 0;
+								for (const ev of mergedEvents) {
+									if (ev.type === Music.DelayEvent) {
+										t += ev.ticks;
+										continue;
+									}
+									ev.test_absTime = t;
+									this.timedEvents.push(ev);
+								}
+							}
+						});
+
+						it(`should have a track of channel type ${channelTypeText}`, function() {
+							const tcAct = music.trackConfig.find(tc => tc.channelType === tcExp.channelType);
+							assert.ok(tcAct, `Missing track with channel type ${channelTypeText}`);
+						});
+
+						// Now we have one big long list of events and times.
+						it(`should have the correct events in ${channelTypeText} channel`, function() {
+							const freq = 440 + transpose[tcExp.channelType];
+							assert.ok(freq);
+
+							const ev = this.timedEvents.find(ev => (
+								(ev.type === Music.NoteOnEvent)
+								&& (ev.frequency > freq - 0.5)
+								&& (ev.frequency < freq + 0.5)
+							));
+
+							assert.ok(ev, 'Missing expected NoteOnEvent');
+							assert.equal(ev.test_absTime, 0, 'NoteOnEvent occurred at wrong time.');
+						});
+
+					});
+				}
+
+				it('should have the expected number of tracks', function() {
+					assert.equal(music.trackConfig.length, defaultMusic.trackConfig.length, 'Wrong number of tracks.');
 				});
 
-				it('should have the standard number of events', function() {
-					assert.equal(music.patterns.length, 1);
-					assert.equal(music.patterns[0].tracks.length, 1, 'Wrong number of tracks.');
-					assert.equal(music.patterns[0].tracks[0].events.length, 6, 'Wrong number of events in first track.');
+				it(`should have ${defaultMusic.patches.length} instruments`, function() {
+					assert.equal(music.patches.length, defaultMusic.patches.length, 'Wrong number of instruments.');
 				});
+
+				for (let i = 0; i < defaultMusic.patches.length; i++) {
+					it(`should match instrument #${i}`, function() {
+						assert.ok(music.patches[i], `Missing patch #${i}.`);
+
+						// Do a string compare first to make debugging easier.
+						assert.equal(music.patches[i].toString(), defaultMusic.patches[i].toString(), `Patch #${i} differs.`);
+						// But use the comparison function too to confirm it also works.
+						assert.ok(defaultMusic.patches[i].equalTo(music.patches[i]), `Patch #${i} differs.`);
+
+						if (md.caps.patchNames) {
+							// These aren't checked by Patch.equalTo() so we check them now.
+							assert.equal(music.patches[i].title || '', defaultMusic.patches[i].title || '', `Patch #${i} title differs.`);
+						} else {
+							assert.equal(music.patches[i].title, null, `Patch title supplied in format that doesn't support patch titles!`);
+						}
+					});
+				}
 
 				const mdTags = Object.keys(md.caps.tags);
 				if (mdTags.length > 0) {
